dd2c8ebda67d6c9a1339d0580da75fb53a1ca97c
Sunny <sunnyggadekar@gmail.com>
2022-08-20 18:32:20 +0530

    remove unit test cases

--- unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -13,23 +13,5 @@ namespace Prime.UnitTests.Services
             Assert.False(result, "1 should not be prime");
         }
 
-        [Theory]
-        [InlineData(-1)]
-        [InlineData(0)]
-        [InlineData(1)]
-        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
-        {
-            var result = PrimeService.IsPrime(value);
-            Assert.False(result, $"{value} should not be prime");
-        }
-
-        [Fact]
-        public void Returns_the_input()
-        {
-            var input = "message";
-            var result = PrimeService.Display(input);
-            Assert.Equal(result, input);
-
-        }
     }
 }
